c863ce5a2658ef597a5d4a23abeb62b2ce64d349
Ayush Agrawal <ayusha@netlink.com>
2019-12-11 13:58:05 +0530

    update ui

---
+++ packages/web/src/views/Dashboard/components/ReadingCard/ReadingCard.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+const useStyles = makeStyles({
+  depositContext: {
+    flex: 1,
+  },
+  textCenter: {
+    textAlign: 'center'
+  },
+  upperCardSubContainer: {
+    marginTop: '10px',
+    alignItems: 'center',
+  },
+  borderRight: {
+    borderRight: '1px solid #dedede',
+    background: '#e8e9ee'
+  },
+  subMargin: {
+    paddingLeft: '25px',
+    height: 'calc(100% - 20px)',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    
+  },
+  subFontSize: {
+    fontSize: '16px',
+    fontWeight: 'bold',
+  },
+  CardHeight: {
+    height: '100%'
+  },
+  CardInnerHeight: {
+    height: '50%'
+  },
+  subcardBtm: {
+    color: 'blue'
+  }
+});
+export default function ReadingCard() {
+  const classes = useStyles();
+  const theme = useTheme();
+  return (
+    <React.Fragment>
+      <div className={classes.CardHeight}>
+        <div className={`${classes.textCenter} ${classes.CardInnerHeight}`}>
+          <Typography component="p" variant="h2" >
+            $3,024.00
+        </Typography>
+          <Typography variant="caption" display="block" gutterBottom>
+            Premium recieved
+        </Typography>
+        </div>
+        <Grid container className={`${classes.upperCardSubContainer} ${classes.CardInnerHeight}`}>
+          <Grid item xs={6} className={`${classes.borderRight} ${classes.subMargin}`}>
+            <Typography component="p" variant="h2" className={classes.subFontSize}  >
+              $3,024.00
+            </Typography>
+            <Typography variant="caption" display="block" >
+              Premium recieved
+            </Typography>
+          </Grid>
+          <Grid item xs={6} className={`${classes.borderRight} ${classes.subMargin}`}>
+            <Typography component="p" variant="h2" className={classes.subFontSize} >
+              $3,024.00
+            </Typography>
+            <Typography variant="caption" display="block" >
+              Premium recieved
+            </Typography>
+          </Grid>
+          <Grid item xs={12} className={classes.textCenter}>
+            <Typography component="p" variant="subtitle1" className={classes.subcardBtm} >
+              8.45% Higher
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
+  );
+}
--- packages/web/src/views/Dashboard/components/index.js
+++ packages/web/src/views/Dashboard/components/index.js
@@ -1,8 +1,3 @@
-export { default as Budget } from './Budget';
-export { default as LatestOrders } from './LatestOrders';
-export { default as LatestProducts } from './LatestProducts';
-export { default as LatestSales } from './LatestSales';
-export { default as TasksProgress } from './TasksProgress';
-export { default as TotalProfit } from './TotalProfit';
-export { default as TotalUsers } from './TotalUsers';
-export { default as UsersByDevice } from './UsersByDevice';
+export { default as ReadingCard } from './ReadingCard';
+export { default as FilterPanel } from './FilterPanel';
+export { default as TitleBar } from './TitleBar';
