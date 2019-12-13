647d9ec3a3ae698296827f1d60ec67d2835fc787
Ayush Agrawal <ayusha@netlink.com>
2019-12-13 10:47:38 +0530

    drawer added

--- packages/web/src/views/Dashboard/components/FilterPanel/FilterPanel.jsx
+++ packages/web/src/views/Dashboard/components/FilterPanel/FilterPanel.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
-import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import { makeStyles } from '@material-ui/core/styles'; import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import FilterBAndWIcon from '@material-ui/icons/FilterBAndW';
+import MailIcon from '@material-ui/icons/Mail';
 const useStyles = makeStyles(theme => ({
     filter: {
         display: 'flex'
@@ -11,16 +19,64 @@ const useStyles = makeStyles(theme => ({
     space: {
         marginRight: '12px'
     },
-    bdypadding : {
+    bdypadding: {
         padding: '15px'
     },
     textRight: {
         textAlign: 'right'
-        
-    }
+
+    },
+    list: {
+        width: 250,
+      },
+      fullList: {
+        width: 'auto',
+      },
 }));
 export default function FilterPanel() {
     const classes = useStyles();
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+    const toggleDrawer = (side, open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [side]: open });
+    };
+    const sideList = side => (
+        <div
+            className={classes.list}
+            role="presentation"
+            onClick={toggleDrawer(side, false)}
+            onKeyDown={toggleDrawer(side, false)}
+        >
+            <div>
+                <FilterBAndWIcon />
+            </div>
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
     return (
         <div >
             <div >
@@ -52,8 +108,12 @@ export default function FilterPanel() {
                         </div>
                     </Grid>
                     <Grid item xs={6} className={classes.textRight}>
-                        <Button variant="outlined"><AddIcon />Add Filters</Button>
+                        <Button onClick={toggleDrawer('right', true)} variant="outlined"><AddIcon />Add Filters</Button>
+                        <Drawer anchor="right" open={state.right} onClose={toggleDrawer('right', false)}>
+                        {sideList('right')}
+                    </Drawer>
                     </Grid>
+                   
                 </Grid>
             </div>
         </div>
--- packages/web/src/views/Dashboard/components/TabExpandView/TabExpandView.js
+++ packages/web/src/views/Dashboard/components/TabExpandView/TabExpandView.js
@@ -5,10 +5,12 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MenuIcon from '@material-ui/icons/Menu';
 const useStyles = makeStyles( theme => ({
     root: {
         width: '100%',
+        position: 'relative',
+        top: '-60px'
     },
     heading: {
         fontSize: theme.typography.pxToRem(15),
@@ -22,10 +24,11 @@ export default function TabExpandView() {
             <div className={classes.root}>
                 <ExpansionPanel>
                     <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
+                        
                         aria-controls="panel1a-content"
                         id="panel1a-header"
                     >
+                        <MenuIcon />
                     </ExpansionPanelSummary>
                     <ExpansionPanelDetails>
                         <Typography>
