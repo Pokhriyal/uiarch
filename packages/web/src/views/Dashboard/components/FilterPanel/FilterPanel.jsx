cae88289ea679c9aaf1243e195c2f9ebd87dfaf6
Ayush Agrawal <ayusha@netlink.com>
2019-12-16 12:45:25 +0530

    filter panel changes

--- packages/web/src/views/Dashboard/components/FilterPanel/FilterPanel.jsx
+++ packages/web/src/views/Dashboard/components/FilterPanel/FilterPanel.jsx
@@ -4,13 +4,11 @@ import Grid from '@material-ui/core/Grid';
 import AddIcon from '@material-ui/icons/Add';
 import Drawer from '@material-ui/core/Drawer';
 import { makeStyles } from '@material-ui/core/styles'; import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
 const useStyles = makeStyles(theme => ({
     filter: {
         display: 'flex'
@@ -33,15 +31,20 @@ const useStyles = makeStyles(theme => ({
         '& button': {
             fontSize: '12px',
             textTransform: 'capitalize',
-            '& svg' :{
+            '& svg': {
                 fontSize: '17px',
                 marginRight: '3px',
             }
         }
 
     },
+    filterHead: {
+        padding: '5px'
+    },
     filterDrawerTop: {
-        padding: '10px',
+        paddingTop: '10px',
+        paddingBottom: '5px',
+        display: 'flex',
         borderBottom: '1px solid #dedede'
     },
     filterDrawerCta: {
@@ -52,7 +55,11 @@ const useStyles = makeStyles(theme => ({
         textAlign: 'right',
         padding: '5px',
     }
+    
 }));
+
+
+
 export default function FilterPanel() {
     const classes = useStyles();
     const [state, setState] = React.useState({
@@ -61,6 +68,14 @@ export default function FilterPanel() {
         bottom: false,
         right: false,
     });
+    const handleChange = name => event => {
+        setState({
+          ...state,
+          [name]: event.target.value,
+        });
+      };
+    const inputLabel = React.useRef(null);
+    const [labelWidth, setLabelWidth] = React.useState(0);
     const toggleDrawer = (side, open) => event => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
@@ -76,17 +91,22 @@ export default function FilterPanel() {
             onKeyDown={toggleDrawer(side, false)}
         >
             <div>
-                <div>
-                    <Typography variant="h4" className={classes.filterDrawerTop}>
-                        Filters
-                    </Typography>
+                <div className={classes.filterDrawerTop}>
+                    <div className={classes.filterHead}>
+                        <FilterListIcon />
+                    </div>
+                    <div className={classes.filterHead}>
+                        <Typography variant="h4">
+                            Filters
+                        </Typography>
+                    </div>
                 </div>
                 <div>
                     Lists
                 </div>
                 <div className={classes.filterDrawerCta}>
                     <div>
-                  
+
                         <Button variant="contained">Cancel</Button>
                         <Button variant="contained">Reset</Button>
                         <Button variant="contained" color="primary">
@@ -131,7 +151,7 @@ export default function FilterPanel() {
                         <Button size="small" color="default" onClick={toggleDrawer('right', true)} variant="contained"><AddIcon />Add Filters</Button>
                         <Drawer anchor="right" open={state.right} onClose={toggleDrawer('right', false)}>
                             {sideList('right')}
-                        </Drawer>
+                        </Drawer>  
                     </Grid>
 
                 </Grid>
